06ad16eabcaf715bbd8fe15d4eb4d00d72bb16b9
Betzabe Silva <betzabesp94@gmail.com>
2022-05-17 00:18:51 -0300

    Fix bindimgs

---
+++ Source/UE_ZirconTech_POC/ScreenPlayer.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ScreenPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_ZIRCONTECH_POC_API UScreenPlayer : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+		class UTextBlock* AddressTextBlock;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+		class UEditableTextBox* AddressTextBox;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+		class UButton* SendButton;
+};
